ae83b6b1504d5916bd9dced4039ec76f6619421e
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-01-23 02:04:48 +0100

    Store with actions, mutations, state, getters created

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,13 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
+import state from "@/store/state";
+import actions from "@/store/actions";
+import getters from "@/store/getters";
+import mutations from "@/store/mutations";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  state,
+  mutations,
+  actions,
+  getters,
+  modules: {},
 });
