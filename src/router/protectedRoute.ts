84ca26d4df06fc0848cff2c502ecc6ead3e494f6
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-01-25 19:46:30 +0100

    ProtectedRoutes component with NavigationGuards was created

---
+++ src/router/protectedRoute.ts
@@ -0,0 +1,28 @@
+import { NavigationGuard } from "vue-router";
+import state from "@/store/state";
+
+const adminProtectedRoute: NavigationGuard = (to, from, next) => {
+  if (state.currentUser.adminAccess === true) {
+    next();
+  } else {
+    next("/login");
+  }
+};
+
+const teacherProtectedRoute: NavigationGuard = (to, from, next) => {
+  if (state.currentUser.teacherAccess === true) {
+    next();
+  } else {
+    next("/login");
+  }
+};
+
+const studentProtectedRoute: NavigationGuard = (to, from, next) => {
+  if (state.currentUser.studentAccess === true) {
+    next();
+  } else {
+    next("/login");
+  }
+};
+
+export { adminProtectedRoute, teacherProtectedRoute, studentProtectedRoute };
