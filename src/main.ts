981b39663f21deff71ad8457f25abd62dbcdb3e7
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-01-26 19:12:01 +0100

    Plugin vue-dk-toast installed, css rules added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { createApp } from "vue";
 import BootstrapVue3 from "bootstrap-vue-3";
+import DKToast from "vue-dk-toast";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 /* import "bootstrap-vue-3/dist/bootstrap-vue-3.css"; */
 
-createApp(App).use(store).use(router).use(BootstrapVue3).mount("#app");
+createApp(App)
+  .use(DKToast, {
+    duration: 2000,
+    positionY: "bottom",
+    positionX: "right",
+    styles: {
+      color: "#CC1810",
+      backgroundColor: "#FFC107",
+      width: "120px",
+      border: "1px solid #CC1810",
+    },
+  })
+  .use(store)
+  .use(router)
+  .use(BootstrapVue3)
+  .mount("#app");
