9c626e1cf3a14ab76c24d583133280d36dd780b7
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-06 22:31:34 +0100

    To action getUserById commit 'lastLoadedUser' added

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -118,6 +118,7 @@ const actions = {
     commit("startLoading");
     const { data } = await axios.get(`${process.env.VUE_APP_URL}/user/get-one-by-id/${userId}`);
     commit("loadedUsersFromGroup", { ...data });
+    commit("lastLoadedUser", { ...data });
     commit("stopLoading");
   },
 
