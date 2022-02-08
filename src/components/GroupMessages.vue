1175ec4f90cd0508103cc5835fd38454a659fe0c
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-08 02:08:41 +0100

    In GroupMessages b-card was changed to full-card component

--- src/components/GroupMessages.vue
+++ src/components/GroupMessages.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="container ck-content">
-    <h2 class="mb-4">Сообщения группы:</h2>
-    <ul v-if="!isLoading" class="list-group-item-success reverseorder">
-      <li v-for="message in currentGroup.homeworkToDo || []" :key="message" class="container__list items-list">
-        <b-card>
-          <div class="mt-3">
-            <b-card-group deck>
-              <b-card bg-variant="light" :header="new Date(message.time).toLocaleString()" class="text-center">
-                <div v-html="message.message"></div>
-              </b-card>
-            </b-card-group>
+    <h2 class="mb-4 text-center">Сообщения группы:</h2>
+    <div v-if="!isLoading" class="row">
+      <div class="col-xl-12 col-lg-12 cards-container ck-content reverseorder">
+        <full-card
+          v-for="homeworkToDo in currentGroup.homeworkToDo || []"
+          :key="homeworkToDo"
+          type="body"
+          icon="fas fa-user-edit"
+          class="mb-4 mt-4 mb-xl-0"
+          :shadow="true"
+          shadowSize="md"
+          :hover="true"
+        >
+          <h2 class="text-center card-title text-uppercase mb-1">{{ homeworkToDo.title }}</h2>
+          <div class="m-2 card-text">
+            <div v-html="homeworkToDo.message"></div>
           </div>
-        </b-card>
-      </li>
-    </ul>
+          <template v-slot:footer>
+            <span class="text-nowrap text-muted footer"
+              >Создано {{ new Date(homeworkToDo.time).toLocaleString() }}.<span class="font-weight-bold"></span
+              ><b-button
+                pill
+                class="button-delete align-self-center btn-light"
+                @click="handleDeleteMessage(homeworkToDo.time), $toast('Сообщение удалено')"
+                ><em class="fa fa-trash-alt"></em>
+              </b-button>
+            </span>
+          </template>
+        </full-card>
+      </div>
+    </div>
     <div v-if="isLoading" class="spinner-border" role="status">
       <span class="visually-hidden">Загружается...</span>
     </div>
@@ -22,12 +39,38 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { mapState } from "vuex";
+import { mapActions, mapState } from "vuex";
+import FullCard from "./FullCard.vue";
+
+interface HomeWorks {
+  message: string;
+  time: string;
+}
 
 export default defineComponent({
   name: "Messages",
+  components: {
+    FullCard,
+  },
   computed: {
     ...mapState(["currentGroup", "isLoading"]),
+  },
+
+  methods: {
+    ...mapActions(["updateGroup", "getGroupById"]),
+    ...mapState([""]),
+
+    async handleDeleteMessage(messageTime: string) {
+      console.log("homeworks: ", this.currentGroup.homeworkToDo);
+      const groupToUpdate = {
+        homeworkToDo: [
+          ...this.currentGroup.homeworkToDo.filter((homework: HomeWorks) => homework.time !== messageTime),
+        ],
+        id: this.currentGroup.id,
+      };
+
+      await this.updateGroup(groupToUpdate);
+    },
   },
 });
 </script>
@@ -42,5 +85,14 @@ export default defineComponent({
 }
 .list-group-item-success {
   background-color: var(--bairro-alto-logo-color);
+}
+.button-delete {
+  font-size: 16px;
+  background-color: transparent;
+  border: transparent;
+}
+.footer {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
