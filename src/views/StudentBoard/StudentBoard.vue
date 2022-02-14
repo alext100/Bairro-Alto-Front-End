75c45790b98bfa940edf29b6a5dae38274a86f5d
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-14 13:43:10 +0100

    StudentBoard.vue moved to 'StudentBoard/'

---
+++ src/views/StudentBoard/StudentBoard.vue
@@ -0,0 +1,30 @@
+<template>
+  <SidebarMenu :menuItems="menuItems" :profileName="profileName" :isExitButton="true" />
+</template>
+
+<script>
+import state from "@/store/state";
+import { defineComponent } from "vue";
+import SidebarMenu from "@/components/SidebarMenu.vue";
+import { mapState } from "vuex";
+import menuItems from "./sideBarStudentMenuItems";
+
+export default defineComponent({
+  name: "StudentBoard",
+  components: {
+    SidebarMenu,
+  },
+
+  data() {
+    return {
+      profileName: state.currentUser.firstName,
+      menuItems: menuItems(),
+    };
+  },
+  computed: {
+    ...mapState(["currentUser"]),
+  },
+});
+</script>
+
+<style lang="scss"></style>
