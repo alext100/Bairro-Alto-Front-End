91e36a9ef7240dc2fd93d1ae2d26cc5880f8fddc
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-18 12:11:59 +0100

    SideBarTeacherMenuItems created

---
+++ src/views/TeacherBoard/sideBarTeacherMenuItems.js
@@ -0,0 +1,30 @@
+export default function sideBarTeacherMenuItems() {
+  return [
+    {
+      link: `/teacher`,
+      name: "Мои группы",
+      icon: "bx-grid-alt",
+    },
+    {
+      link: `/teacher/create-group`,
+      name: "Создать группу",
+      icon: "bx-add-to-queue",
+    },
+    {
+      link: `/teacher/create-lesson`,
+      name: "Создать урок",
+      icon: "bx-laptop",
+    },
+    {
+      link: `/teacher/chat/`,
+      name: "Чат",
+      tooltip: "Чат",
+      icon: "bx-chat",
+    },
+    {
+      link: `/teacher`,
+      name: "Settings",
+      icon: "bx-cog",
+    },
+  ];
+}
