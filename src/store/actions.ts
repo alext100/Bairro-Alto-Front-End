328fe35fdeb64d3af8fa9c77e94e035f066f0222
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-03-13 21:11:31 +0100

    SignInAnonymously function was refactored

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -28,13 +28,13 @@ const actions = {
     dispatch("userLogedFromApi", { user, token });
     localStorage.setItem("userData", JSON.stringify({ token }));
 
-    signInAnonymously(auth)
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
+    signInAnonymously(auth);
+    /*     // eslint-disable-next-line @typescript-eslint/no-empty-function
       .then(() => {})
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-      });
+      }) */
 
     commit("stopLoading");
   },
