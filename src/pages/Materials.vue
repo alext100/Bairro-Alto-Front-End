26a9cb07ccc84464d6efac40c6a8d11a9ff81aed
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-03-16 05:11:27 +0100

    Materials page created

---
+++ src/pages/Materials.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="materials-page__features ck-content">
+      <h1 class="features-title features-item m-0">Материалы</h1>
+      <div class="container cards-container">
+        <n-card hoverable v-for="material in materials || []" :key="material" footer-style="font-size:18px">
+          <template #cover>
+            <router-link :to="{ name: 'MaterialsPost', params: { id: material.id } }">
+              <img :src="material.image" />
+            </router-link>
+          </template>
+          <template #footer>
+            <router-link class="materials-page__router-link" :to="'/materials/' + material.id">
+              {{ material.title }}
+            </router-link>
+          </template>
+        </n-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, onMounted } from "vue";
+import { mapActions, useStore } from "vuex";
+import { Post } from "@/types/interfaces";
+import { NCard } from "naive-ui";
+
+export default defineComponent({
+  name: "Materials",
+  components: { NCard },
+
+  setup() {
+    const { state, dispatch } = useStore();
+    onMounted(() => {
+      dispatch("getWebContent");
+      document.body.style.backgroundColor = "white";
+    });
+
+    const materials = computed(() => state.webContent?.posts?.filter((post: Post) => post?.category === "Материалы"));
+
+    return {
+      materials,
+    };
+  },
+
+  methods: {
+    ...mapActions(["getWebContent"]),
+  },
+});
+</script>
+
+<style scoped>
+.n-card {
+  width: 300px;
+  height: 400px;
+  margin: 10px 3px 10px 3px;
+}
+.cards-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.materials-page__router-link {
+  color: black;
+  text-decoration: none;
+}
+</style>
