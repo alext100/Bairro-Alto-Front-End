b563155a2281654e6754a9613b7292442a350e64
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-03-17 03:10:52 +0100

    CoursesNewSchool.vue page created

---
+++ src/pages/CoursesNewSchool.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container ck-content m-4">
+    <n-card :bordered="false" hoverable>
+      <div v-html="coursesSchool[0].body"></div>
+    </n-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { Post } from "@/types/interfaces";
+import { computed, defineComponent, onMounted } from "vue";
+import { useStore } from "vuex";
+import { NCard } from "naive-ui";
+
+export default defineComponent({
+  name: "CoursesNewSchool",
+  components: { NCard },
+
+  setup() {
+    const { state, dispatch } = useStore();
+    onMounted(() => {
+      dispatch("getWebContent");
+      document.body.style.backgroundColor = "white";
+    });
+    const coursesSchool = computed(() =>
+      state.webContent?.posts?.filter((post: Post) => post?.title.match("Офлайн ДЛЯ НАЧИНАЮЩИХ"))
+    );
+
+    return {
+      coursesSchool,
+    };
+  },
+});
+</script>
