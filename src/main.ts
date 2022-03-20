e59872f9a9f036a86cf94ee634dbe3b7dbe3d221
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-03-20 21:17:22 +0100

    Component vue-next-select installed and imported

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import Particles from "particles.vue3";
 import VueMobileDetection from "vue-mobile-detection";
 import VueKinesis from "vue-kinesis";
 import { MotionPlugin } from "@vueuse/motion";
+import VueNextSelect from "vue-next-select";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -13,6 +14,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "boxicons/css/boxicons.min.css";
+import "vue-next-select/dist/index.min.css";
 
 createApp(App)
   .use(DKToast, {
@@ -34,4 +36,5 @@ createApp(App)
   .use(VueMobileDetection)
   .use(VueKinesis)
   .use(MotionPlugin)
+  .component("vue-select", VueNextSelect)
   .mount("#app");
