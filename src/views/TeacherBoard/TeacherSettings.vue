d9d2db518737207d3f4745f7acb300d77e2d95ae
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-03-28 21:17:26 +0200

    TeacherSettings view created

---
+++ src/views/TeacherBoard/TeacherSettings.vue
@@ -0,0 +1,29 @@
+<template>
+  <SidebarMenu :menuItems="menuItems" :profileName="profileName" :isExitButton="true" />
+  <ChangePassword />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import SidebarMenu from "@/components/SidebarMenu.vue";
+import sidebarTeacherMenuItems from "@/views/TeacherBoard/sideBarTeacherMenuItems";
+import ChangePassword from "@/components/ChangePassword.vue";
+
+export default defineComponent({
+  name: "TeacherSettings",
+  components: {
+    SidebarMenu,
+    ChangePassword,
+  },
+  setup() {
+    const { state } = useStore();
+
+    return {
+      currentUser: state.currentUser,
+      menuItems: sidebarTeacherMenuItems(),
+      profileName: state.currentUser.firstName,
+    };
+  },
+});
+</script>
