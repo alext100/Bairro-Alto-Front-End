f8529002d2fe05f27484eeee96a592316ffffba4
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-03-28 21:17:05 +0200

    StudentSettings view created

---
+++ src/views/StudentBoard/StudentSettings.vue
@@ -0,0 +1,29 @@
+<template>
+  <SidebarMenu :menuItems="menuItems" :profileName="profileName" :isExitButton="true" />
+  <ChangePassword />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import SidebarMenu from "@/components/SidebarMenu.vue";
+import ChangePassword from "@/components/ChangePassword.vue";
+import sideBarStudentMenuItems from "./sideBarStudentMenuItems";
+
+export default defineComponent({
+  name: "StudentSettings",
+  components: {
+    SidebarMenu,
+    ChangePassword,
+  },
+  setup() {
+    const { state } = useStore();
+
+    return {
+      currentUser: state.currentUser,
+      menuItems: sideBarStudentMenuItems(),
+      profileName: state.currentUser.firstName,
+    };
+  },
+});
+</script>
