76bc470ec19ff255dd5d607c05d10d2e83712c31
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-04-02 14:35:14 +0200

    MockedState to '/tests' added

---
+++ tests/mockedState.ts
@@ -0,0 +1,52 @@
+import { State } from "@/types/interfaces";
+
+const mockedState = {
+  currentUser: localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData") || "") : "",
+  // eslint-disable-next-line no-unneeded-ternary
+  isUserAuthenticated: localStorage.getItem("userData") ? true : false,
+  groups: [],
+  userGroups: [],
+  currentGroup: {},
+  loadedUsersFromGroup: [],
+  groupErrors: [],
+  allUsers: [],
+  isRegistered: false,
+  isWrongEmailOnRegister: false,
+  isLoading: false,
+  userId: "",
+  token: "",
+  refreshToken: "",
+  firstName: "",
+  lastName: "",
+  user: {
+    id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    adminAccess: false,
+    teacherAccess: false,
+    studentAccess: false,
+    groups: [],
+    image: "",
+    homeworkToCheck: [],
+    teacherGroups: [],
+    studentGroups: [],
+    info: [],
+  },
+  lastLoadedUser: {},
+  lessons: [],
+  groupLessons: [],
+  webContent: sessionStorage.getItem("webContent")
+    ? JSON.parse(sessionStorage.getItem("webContent") || "")
+    : {
+        posts: [],
+        categories: [],
+      },
+  courseNames: sessionStorage.getItem("courseNames")
+    ? JSON.parse(sessionStorage.getItem("courseNames") || "")
+    : {
+        courseNames: [],
+      },
+};
+export default mockedState as unknown as State;
