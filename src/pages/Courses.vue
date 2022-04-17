521c7018071ebfe07812de6e1be9f125c6c3f3d0
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-04-17 15:08:35 +0200

    Courses.vue was refactored to use script setup syntax

--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -7,30 +7,19 @@
   <Footer />
 </template>
 
-<script lang="ts">
-import { Post } from "@/types/interfaces";
-import { computed, defineComponent, onBeforeMount, onMounted } from "vue";
+<script setup lang="ts">
 import { useStore } from "vuex";
 import { NCard } from "naive-ui";
+import { Post } from "@/types/interfaces";
 import Footer from "@/components/Footer.vue";
+import { computed, onBeforeMount, onMounted } from "vue";
 
-export default defineComponent({
-  name: "Courses",
-  components: { NCard, Footer },
+const { state, dispatch } = useStore();
 
-  setup() {
-    const { state, dispatch } = useStore();
-    onBeforeMount(() => dispatch("getWebContent"));
-    onMounted(() => {
-      document.body.style.backgroundColor = "white";
-    });
-    const courses = computed(() =>
-      state.webContent?.posts?.filter((post: Post) => post?.title.match("ОБУЧЕНИЕ В ШКОЛЕ"))
-    );
-
-    return {
-      courses,
-    };
-  },
+onBeforeMount(() => dispatch("getWebContent"));
+onMounted(() => {
+  document.body.style.backgroundColor = "white";
 });
+
+const courses = computed(() => state.webContent?.posts?.filter((post: Post) => post?.title.match("ОБУЧЕНИЕ В ШКОЛЕ")));
 </script>
