ec8470fd7b70c68dc7b582f1359db6fdd4880abf
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-04-25 01:22:13 +0200

    Composable usePixiGlitchFilter was refactored

--- src/composables/usePixiGlitchFilter.ts
+++ src/composables/usePixiGlitchFilter.ts
@@ -1,98 +1,125 @@
+/* eslint-disable no-unused-expressions */
 import { gsap } from "gsap";
 import * as PIXI from "pixi.js";
 import * as filters from "pixi-filters";
 import { GlitchFilterOptions } from "pixi-filters";
-import { computed, ComputedRef, onUnmounted, Ref, ref } from "vue";
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { computed, ComputedRef, onUnmounted } from "vue";
 
-export default function usePixiGlitchFilter(
+const getImageDimensions = async (imageUrl: string): Promise<{ width: number; height: number }> =>
+  new Promise((resolve) => {
+    const image = new Image();
+    image.src = imageUrl;
+    let dimensiones = { width: 800, height: 600 };
+    image.onload = () => {
+      dimensiones = {
+        width: image.width,
+        height: image.height,
+      };
+      resolve(dimensiones);
+    };
+  });
+
+const updateSize = (): void => {
+  const canvas: HTMLCanvasElement | null = document.body.querySelector("canvas");
+
+  if (canvas && window.innerWidth < 1100) {
+    canvas?.setAttribute("style", "max-width: 850px");
+  }
+  if (canvas && window.innerWidth < 992) {
+    canvas?.setAttribute("style", "width: 630px");
+  }
+  if (canvas && window.innerWidth < 768) {
+    canvas?.setAttribute("style", "width: 450px");
+  }
+  if (canvas && window.innerWidth < 600) {
+    canvas?.setAttribute("style", "width: 400px");
+  }
+  if (canvas && window.innerWidth < 500) {
+    canvas?.setAttribute("style", "width: 300px");
+  }
+  window.addEventListener("resize", updateSize);
+};
+
+const usePixiGlitchFilter = (
   querySelectorOfImageToReplace: string,
-  htmlStringWithImageToParse: string,
-  cardCoverImageSrcAttribute: string
-) {
+  htmlStringWithImage: string,
+  cardCoverImageSrc: string
+) => {
   if (!PIXI.utils.isMobile.any) {
+    updateSize();
     const parser = new DOMParser();
-    const documentOfParsedPostBody: Document = parser.parseFromString(htmlStringWithImageToParse, "text/html");
+    const documentOfParsedPostBody: Document = parser.parseFromString(htmlStringWithImage, "text/html");
     const cardCoverImage: HTMLElement | null = documentOfParsedPostBody.querySelector(querySelectorOfImageToReplace);
     const imageToReplace: ComputedRef<HTMLElement> = computed(
       () => document.body.querySelector(querySelectorOfImageToReplace) as HTMLElement
     );
 
-    const cardContainer: Ref<HTMLElement | undefined> = ref<HTMLElement>();
-    const app: PIXI.Application = new PIXI.Application({
-      resizeTo: cardContainer.value,
-      backgroundAlpha: 0,
-    });
-    const container = new PIXI.Container();
-    app.stage.addChild(container);
+    let app: PIXI.Application;
+    let container: PIXI.Container;
+    let filter: PIXI.Filter;
 
-    queueMicrotask(() => {
+    queueMicrotask(async () => {
+      const srcUrlImageToReplace = imageToReplace.value.getAttribute(cardCoverImageSrc)?.split(" ")[0] as string;
+      const { width, height } = await getImageDimensions(srcUrlImageToReplace);
+
+      app = new PIXI.Application({
+        width: width < 880 ? width : 880,
+        height,
+        backgroundAlpha: 0,
+        autoDensity: true,
+      });
+
+      container = new PIXI.Container();
+      app.stage.addChild(container);
+
+      const filterOptions: Pick<GlitchFilterOptions, "slices"> = { slices: 200 };
+      filter = new filters.GlitchFilter(filterOptions);
+      container.filters = [filter];
+
       if (imageToReplace.value && cardCoverImage) {
-        const spriteImage = `${cardCoverImage?.getAttribute(cardCoverImageSrcAttribute)}`;
-        if (cardCoverImageSrcAttribute === "src") {
-          const pixiSprite = PIXI.Sprite.from(spriteImage);
+        const spriteImageUrl = `${cardCoverImage?.getAttribute(cardCoverImageSrc)}`;
+        if (cardCoverImageSrc === "src") {
+          const pixiSprite = PIXI.Sprite.from(spriteImageUrl);
           container.addChild(pixiSprite);
         }
-        if (cardCoverImageSrcAttribute === "srcset") {
-          const pixiSprite = PIXI.Sprite.from(spriteImage.split(" ")[0]);
+        if (cardCoverImageSrc === "srcset") {
+          const pixiSprite = PIXI.Sprite.from(spriteImageUrl.split(" ")[0]);
           container.addChild(pixiSprite);
         }
-
         imageToReplace.value.replaceWith(app.view);
       }
-    });
 
-    const filterOptions: Pick<GlitchFilterOptions, "slices"> = { slices: 200 };
-    const filter = new filters.GlitchFilter(filterOptions as any);
-
-    container.filters = [filter] as any;
-
-    gsap.fromTo(
-      filter,
-      {
-        offset: 0,
-      },
-      {
-        offset: 1000,
-        duration: 2,
-        ease: "power2.inOut",
-        yoyo: true,
-        repeat: 1,
-        repeatDelay: 1,
-      }
-    );
-    gsap.fromTo(
-      filter,
-      {
-        direction: 0,
-      },
-      {
-        direction: 360,
-        duration: 30,
-        ease: "none",
-        repeat: 1,
-      }
-    );
-
-    const updateSize = (): void => {
-      container.x = -100;
-      container.y = 0;
-      if (window.innerWidth < 1000) {
-        container.x = -150;
-      }
-      if (window.innerWidth < 800) {
-        container.x = -200;
-      }
-      if (window.innerWidth < 700) {
-        container.x = -300;
-      }
-    };
-
-    window.addEventListener("resize", updateSize);
-    updateSize();
-
-    onUnmounted(() => {
-      window.removeEventListener("resize", updateSize);
+      gsap.fromTo(
+        filter,
+        {
+          offset: 0,
+        },
+        {
+          offset: 1000,
+          duration: 2,
+          ease: "power2.inOut",
+          yoyo: true,
+          repeat: 1,
+          repeatDelay: 1,
+        }
+      );
+      gsap.fromTo(
+        filter,
+        {
+          direction: 0,
+        },
+        {
+          direction: 360,
+          duration: 30,
+          ease: "none",
+          repeat: 1,
+        }
+      );
     });
   }
-}
+  onUnmounted(() => {
+    window.removeEventListener("resize", updateSize);
+  });
+};
+
+export default usePixiGlitchFilter;
