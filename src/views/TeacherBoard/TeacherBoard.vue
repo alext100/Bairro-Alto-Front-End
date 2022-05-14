dfdf4b042f14c4abef1076f65364f042b351e58f
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-05-14 20:31:29 +0200

    Bootstrap-vue components was replaced by NaiveUI components

--- src/views/TeacherBoard/TeacherBoard.vue
+++ src/views/TeacherBoard/TeacherBoard.vue
@@ -1,70 +1,75 @@
 <template>
   <SidebarMenu :menuItems="menuItems" :profileName="profileName" :isExitButton="true" />
-  <div class="container">
-    <h1 class="m-2">Мои группы</h1>
-    <b-card border-variant="white" class="teacher-card" style="min-width: 300px">
-      <b-card-text
-        >В этом разделе можно найти все группы школы и добавить группы в твой список или удалить их оттуда. Перейдя в
-        конкретную группу можно добавить или удалить студентов из группы. Назначить им домашнее задание. Или делать
-        заметки во время онлайн урока.</b-card-text
-      >
-    </b-card>
+  <div class="teacher-board container d-flex flex-column align-items-center">
+    <n-h1 class="m-2">Мои группы</n-h1>
+    <n-card
+      content-style="padding: 16px; font-size: 16px"
+      class="teacher-board__teacher-card"
+      style="min-width: 300px"
+      :bordered="false"
+      size="huge"
+    >
+      В этом разделе можно найти все группы школы и добавить группы в твой список или удалить их оттуда. Перейдя в
+      конкретную группу можно добавить или удалить студентов из группы. Назначить им домашнее задание. Или делать
+      заметки во время онлайн урока.
+    </n-card>
     <div class="container">
-      <ul class="list-group-item-success">
-        <li v-for="group in userGroups || []" :key="group" class="d-flex flex-column items-list">
-          <b-card>
+      <ul class="teacher-board__list-group-item--success">
+        <li v-for="group in userGroups || []" :key="group" class="d-flex flex-column">
+          <n-card>
             <div class="mt-3">
-              <b-card-group deck>
-                <b-card bg-variant="light" class="text-center">
-                  <router-link :to="'/group/' + group.id"
-                    ><b-card-text>{{ group.groupName }}</b-card-text>
-                  </router-link>
-
-                  <template #footer>
-                    <b-button
-                      v-tippy="'Удалить группу из моего списка'"
-                      @click="handleDeleteGroupe(group.id), $toast('Удалено из твоих групп')"
-                      pill
-                      class="btn-lg button-delete align-self-center"
-                      ><em class="far fa-trash-alt"></em>
-                    </b-button>
-                  </template>
-                </b-card>
-              </b-card-group>
+              <n-list bordered>
+                <n-layout embedded content-style="padding: 14px;">
+                  <n-card :hoverable="true" class="text-center" content-style="font-size: 18px;">
+                    <router-link :to="'/group/' + group.id">
+                      {{ group.groupName }}
+                    </router-link>
+                    <template #footer>
+                      <button
+                        v-tippy="'Удалить группу из моего списка'"
+                        @click="handleDeleteGroupe(group.id), $toast('Удалено из твоих групп')"
+                        class="btn-lg teacher-board__button-delete align-self-center"
+                      >
+                        <em class="far fa-trash-alt"></em>
+                      </button>
+                    </template>
+                  </n-card>
+                </n-layout>
+              </n-list>
             </div>
-          </b-card>
+          </n-card>
         </li>
       </ul>
     </div>
-
-    <b-button v-on:click="isHiddenFormToCreate = !isHiddenFormToCreate" pill class="b-card__button mb-5 mt-5 w-100">{{
-      isHiddenFormToCreate ? "Показать все группы" : "Скрыть список групп"
-    }}</b-button>
-
+    <SubmitButton v-on:click="isHiddenFormToCreate = !isHiddenFormToCreate" class="mb-5 mt-5 w-75">
+      {{ isHiddenFormToCreate ? "Показать все группы" : "Скрыть список групп" }}
+    </SubmitButton>
     <div v-if="!isHiddenFormToCreate" class="container">
-      <h2>Все группы:</h2>
-      <ul class="list-group-item-success">
-        <li v-for="group in groups || []" :key="group" class="d-flex flex-column items-list">
-          <b-card>
+      <n-h2 class="m-2">Все группы:</n-h2>
+      <ul class="teacher-board__list-group-item--success">
+        <li v-for="group in groups || []" :key="group" class="d-flex flex-column">
+          <n-layout>
             <div class="mt-3">
-              <b-card-group deck>
-                <b-card bg-variant="light" class="text-center">
-                  <router-link :to="'/group/' + group.id">
-                    <b-card-text>{{ group.groupName }}</b-card-text>
-                  </router-link>
-                  <template #footer>
-                    <b-button
-                      v-tippy="'Добавить группу в мой список'"
-                      @click="handleAddToMyGroups(group.id), $toast('Добавлено в твои группы')"
-                      pill
-                      class="btn-lg button-delete align-self-center"
-                      ><em class="bi bi-plus-circle"></em>
-                    </b-button>
-                  </template>
-                </b-card>
-              </b-card-group>
+              <n-list bordered>
+                <n-layout embedded content-style="padding: 14px;">
+                  <n-card :hoverable="true" class="text-center" content-style="font-size: 18px;">
+                    <router-link :to="'/group/' + group.id">
+                      {{ group.groupName }}
+                    </router-link>
+                    <template #footer>
+                      <button
+                        v-tippy="'Добавить группу в мой список'"
+                        @click="handleAddToMyGroups(group.id), $toast('Добавлено в твои группы')"
+                        class="btn-lg teacher-board__button-add align-self-center"
+                      >
+                        <em class="bi bi-plus-circle"></em>
+                      </button>
+                    </template>
+                  </n-card>
+                </n-layout>
+              </n-list>
             </div>
-          </b-card>
+          </n-layout>
         </li>
       </ul>
     </div>
@@ -72,15 +77,17 @@
 </template>
 
 <script lang="ts">
+import state from "@/store/state";
 import { defineComponent } from "vue";
 import { mapActions, mapState } from "vuex";
 import SidebarMenu from "@/components/SidebarMenu.vue";
-import state from "@/store/state";
+import SubmitButton from "@/components/SubmitButton.vue";
+import { NCard, NH1, NH2, NList, NLayout } from "naive-ui";
 import sidebarTeacherMenuItems from "@/views/TeacherBoard/sideBarTeacherMenuItems";
 
 export default defineComponent({
   name: "TeacherBoard",
-  components: { SidebarMenu },
+  components: { NH1, NH2, NList, NCard, NLayout, SidebarMenu, SubmitButton },
 
   computed: {
     ...mapState(["currentUser", "groups", "userGroups"]),
@@ -93,12 +100,6 @@ export default defineComponent({
     async handleDeleteGroupe(groupID: string) {
       await this.deleteUserGroup(groupID);
     },
-    /*     handleLogout(event: Event) {
-      if ((event.target as HTMLInputElement).classList.contains("profile__log_out")) {
-        this.deleteDataFromLocalStorage();
-        this.$router.push("/");
-      }
-    }, */
   },
   mounted() {
     this.getGroupsFromApi();
@@ -115,27 +116,29 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.b-card__button {
-  min-width: fit-content;
-}
-.button-delete {
-  background-color: inherit;
-  background-color: transparent;
-  color: currentColor;
-  border: none;
-  width: fit-content;
+.teacher-board {
+  &__teacher-card {
+    font-family: "Tenor-sans", sans-serif;
+    text-align: justify;
+    padding: 17px;
+  }
+  &__list-group-item--success {
+    background-color: var(--bairro-alto-logo-color);
+  }
+  &__button-delete,
+  &__button-add {
+    background-color: inherit;
+    background-color: transparent;
+    color: currentColor;
+    border: none;
+    width: fit-content;
+  }
 }
 
-.items-list {
-  list-style: none;
+.n-card a {
+  text-decoration: none;
 }
-.list-group-item-success {
-  background-color: var(--bairro-alto-logo-color);
-}
-.teacher-card {
-  font-size: 20px;
-  background-color: transparent;
-  font-family: "Tenor-sans", sans-serif;
-  text-align: justify;
+.container ul {
+  padding-left: 1rem;
 }
 </style>
